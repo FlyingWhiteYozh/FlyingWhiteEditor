cb50535a4d6ccb9a468e4f001aab27b3a1842e7d
Yaroslav <FlyingWhiteYozh@gmail.com>
2017-04-05 14:56:00 +0300

    Форматирование

--- core.php
+++ core.php
@@ -1,87 +1,108 @@
 <?php
 
-require dirname(__FILE__).'/conf.php';
+require dirname(__FILE__) . '/conf.php';
 
-if (!Conf::accessCheck())
-	die('access denied');
+if (!Conf::accessCheck()) {
+    die('access denied');
+}
 
 ob_start('fwe_output_callback');
 
 function fwe_output_callback($content)
 {
-	if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') return false;
+    if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
+        return false;
+    }
 
-	return str_replace('{content}', $content, file_get_contents(Conf::ROOT() . 'modal.html'));
+    return str_replace('{content}', $content, file_get_contents(Conf::ROOT() . 'modal.html'));
 }
 
 function error($message)
 {
-	die('<div class="alert alert-danger">' . htmlspecialchars($message) . '</div>');
+    die('<div class="alert alert-danger">' . htmlspecialchars($message) . '</div>');
 }
 
 function convertUTFtoWIN1251(&$var)
 {
-	if(is_string($var))
-		$var = mb_convert_encoding($var, 'windows-1251', 'UTF-8');
-	if(is_array($var))
-		foreach ($var as &$value) 
-			convertUTFtoWIN1251($value);
+    if (is_string($var)) {
+        $var = mb_convert_encoding($var, 'windows-1251', 'UTF-8');
+    }
+
+    if (is_array($var)) {
+        foreach ($var as &$value) {
+            convertUTFtoWIN1251($value);
+        }
+    }
+
 }
 
 require 'page.php';
 $allowedActions = array('get', 'set');
-$errors = array();
+$errors         = array();
 
-if(empty($_REQUEST['a']) || !in_array($_REQUEST['a'], $allowedActions))  
-	$errors[] = 'Action is not allowed';
+if (empty($_REQUEST['a']) || !in_array($_REQUEST['a'], $allowedActions)) {
+    $errors[] = 'Action is not allowed';
+}
+
 $action = $_REQUEST['a'];
 
+if ($action == 'set') {
+    if (!isset($_REQUEST['data'])) {
+        error('You must provide "data" for this method');
+    }
 
-if($action == 'set') {
-	if(!isset($_REQUEST['data'])) error('You must provide "data" for this method');
-	$data = $_REQUEST['data'];
+    $data = $_REQUEST['data'];
 }
 
-if(Conf::isWIN1251()){
-	convertUTFtoWIN1251($data);
+if (Conf::isWIN1251()) {
+    convertUTFtoWIN1251($data);
 }
 
-if(empty($_REQUEST['id'])) {
-	if($action == 'set') {
-		$pages = array();
-		foreach($_REQUEST['data'] as $pageData){
-			if(isset($pageData['id'])) 
-				$pages[] = new Page($pageData['id']);
-			else
-				$errors[] = 'ID isn\'t specified';
-		}
-	} 
-	else 
-		$errors[] = 'ID isn\'t specified';
+if (empty($_REQUEST['id'])) {
+    if ($action == 'set') {
+        $pages = array();
+        foreach ($_REQUEST['data'] as $pageData) {
+            if (isset($pageData['id'])) {
+                $pages[] = new Page($pageData['id']);
+            } else {
+                $errors[] = 'ID isn\'t specified';
+            }
+
+        }
+    } else {
+        $errors[] = 'ID isn\'t specified';
+    }
+
 }
 
-if(count($errors)) error(implode('<br>'.PHP_EOL, $errors));	
+if (count($errors)) {
+    error(implode('<br>' . PHP_EOL, $errors));
+}
 
-if(!isset($pages)){
-	$ids = (array)$_REQUEST['id'];
-	$pages = array();
-	foreach($ids as $id)
-		$pages[] = new Page($id);
+if (!isset($pages)) {
+    $ids   = (array) $_REQUEST['id'];
+    $pages = array();
+    foreach ($ids as $id) {
+        $pages[] = new Page($id);
+    }
+
 }
 
 switch ($action) {
-	case 'get':
-		foreach($pages as $page)
-			$page->get();
-		break;
-	case 'set':
-		foreach($pages as $page)
+    case 'get':
+        foreach ($pages as $page) {
+            $page->get();
+        }
 
-			$page->update($data[$page->idToString()]);
-		break;
-	
-	default:
-		error('Not allowed');
-		break;
+        break;
+    case 'set':
+        foreach ($pages as $page) {
+            $page->update($data[$page->idToString()]);
+        }
+
+        break;
+
+    default:
+        error('Not allowed');
+        break;
 }
-
--- page.php
+++ page.php
@@ -2,99 +2,112 @@
 
 class Page
 {
-	private $type, $id;
-	public function __construct($id)
-	{
-		if(!strpos($id, ':')) error('Malformed ID');
+    private $type, $id;
+    public function __construct($id)
+    {
+        if (!strpos($id, ':')) {
+            error('Malformed ID');
+        }
 
-		list($typeName, $id) = explode(':', $id);
+        list($typeName, $id) = explode(':', $id);
 
-		$this->type = Types::get($typeName);
-		if(!$this->type) error('Type "' . $typeName . '" is not allowed');
+        $this->type = Types::get($typeName);
+        if (!$this->type) {
+            error('Type "' . $typeName . '" is not allowed');
+        }
 
-		$this->id = explode('-', $id);
-		if(count($this->type->id) != count($this->id)) error('Malformed ID ' . count($this->type->id) . '/' . count($this->id));
-	}
+        $this->id = explode('-', $id);
+        if (count($this->type->id) != count($this->id)) {
+            error('Malformed ID ' . count($this->type->id) . '/' . count($this->id));
+        }
 
-	public function update($vars)
-	{
-		
-		$sqlInsertFields = array();
-		$sqlPlaceholders = array();
-		$sqlUpdateFields = array();
-		$sqlValues = array();
-		
-		foreach(array_keys($this->type->fields) as $fieldKey) {
-			if(!isset($vars[$fieldKey])) error('Field "' . $fieldKey . '" not found');
-			$sqlInsertFields[] = $fieldKey;
-			$sqlPlaceholders[] = ':' . $fieldKey;
-			$sqlUpdateFields[] = $fieldKey . ' = :' . $fieldKey;
-			$sqlValues[$fieldKey] = $vars[$fieldKey];
-		}
-		// $sqlValues[$this->type->id] = $this->id;
-		if($this->type->shouldInsert) {
-			
-			foreach($this->type->id as $n => $fieldKey) {
-				$sqlInsertFields[] = $fieldKey;
-				$sqlPlaceholders[] = ':' . $fieldKey;
-				$sqlUpdateFields[] = $fieldKey . ' = :' . $fieldKey;
-				$sqlValues[$fieldKey] = $this->id[$n];
-			}
-			$sql = 'INSERT INTO ' . $this->type->table . ' (' . implode(', ', $sqlInsertFields) . ') VALUES (' . implode(', ', $sqlPlaceholders) . ') ON DUPLICATE KEY UPDATE ' . implode(', ', $sqlUpdateFields);
-		}
-		else 
-			$sql = 'UPDATE ' . $this->type->table . ' SET ' . implode(', ', $sqlUpdateFields) . $this->getWhereClause($sqlValues);
-		$stmt = prepare($sql);
-		// var_dump($sql, $sqlValues, $this->type);
-		if($stmt->execute($sqlValues)) {
-			echo '<div class="alert alert-success">OK! ' . $stmt->rowCount() . ' rows affected.</div>';
-			// var_dump($this);
-			$this->get();
-		} else {
-			error(var_export($stmt->errorInfo(), 1));
-		}
-	}
+    }
 
-	public function get()
-	{
-		$content = '<hr><div class="form"><p>' . $this->idToString() . '</p><input type="hidden" name="data[' . $this->idToString() . '][id]" class="id" value="' . $this->idToString() . '">';
-		$sqlValues = array();
-		$sql = 'SELECT ' . implode(',', array_keys($this->type->fields)) . ' FROM ' . $this->type->table . $this->getWhereClause($sqlValues);
-		$stmt = prepare($sql);
-		// var_dump($stmt);
-		$result = $stmt->execute($sqlValues);
-		if(!$result) {
-			error(var_export($stmt->errorInfo(), 1));
-		}
-		$data = $stmt->fetch(PDO::FETCH_ASSOC);
-		if(!$data && !$this->type->shouldInsert) error('Page not found');
-		
-		foreach($this->type->fields as $fieldKey=>$field) {
-			$content .= '<div class="form-group"><label for="' . $fieldKey . '">' . $field->name . '</label> <textarea class="form-control" rows="1" name="data[' . $this->idToString() . '][' . $fieldKey . ']" id="' . $fieldKey . '">' . htmlspecialchars($data[$fieldKey]) . '</textarea></div>';
-		}
-		$content .= '</div>';
-		// var_dump($this->type);
-		echo $content;
-	}
+    public function update($vars)
+    {
 
-	private function getWhereClause(&$values)
-	{
-		$idFields = $this->type->id;
-		$idValues = $this->id;
+        $sqlInsertFields = array();
+        $sqlPlaceholders = array();
+        $sqlUpdateFields = array();
+        $sqlValues       = array();
 
-		$placeholders = array();
+        foreach (array_keys($this->type->fields) as $fieldKey) {
+            if (!isset($vars[$fieldKey])) {
+                error('Field "' . $fieldKey . '" not found');
+            }
 
-		foreach($idFields as $k=>$field) {
-			$placeholders[] = $field . ' = :' . $field;
-			$values[$field] = $idValues[$k];
-		}
+            $sqlInsertFields[]    = $fieldKey;
+            $sqlPlaceholders[]    = ':' . $fieldKey;
+            $sqlUpdateFields[]    = $fieldKey . ' = :' . $fieldKey;
+            $sqlValues[$fieldKey] = $vars[$fieldKey];
+        }
+        // $sqlValues[$this->type->id] = $this->id;
+        if ($this->type->shouldInsert) {
 
-		return ' WHERE ' . implode(' && ', $placeholders);
-	}
+            foreach ($this->type->id as $n => $fieldKey) {
+                $sqlInsertFields[]    = $fieldKey;
+                $sqlPlaceholders[]    = ':' . $fieldKey;
+                $sqlUpdateFields[]    = $fieldKey . ' = :' . $fieldKey;
+                $sqlValues[$fieldKey] = $this->id[$n];
+            }
+            $sql = 'INSERT INTO ' . $this->type->table . ' (' . implode(', ', $sqlInsertFields) . ') VALUES (' . implode(', ', $sqlPlaceholders) . ') ON DUPLICATE KEY UPDATE ' . implode(', ', $sqlUpdateFields);
+        } else {
+            $sql = 'UPDATE ' . $this->type->table . ' SET ' . implode(', ', $sqlUpdateFields) . $this->getWhereClause($sqlValues);
+        }
 
-	public function idToString()
-	{
-		return $this->type->name . ':' . implode('-',$this->id);
-	}
+        $stmt = prepare($sql);
+        // var_dump($sql, $sqlValues, $this->type);
+        if ($stmt->execute($sqlValues)) {
+            echo '<div class="alert alert-success">OK! ' . $stmt->rowCount() . ' rows affected.</div>';
+            // var_dump($this);
+            $this->get();
+        } else {
+            error(var_export($stmt->errorInfo(), 1));
+        }
+    }
+
+    public function get()
+    {
+        $content   = '<hr><div class="form"><p>' . $this->idToString() . '</p><input type="hidden" name="data[' . $this->idToString() . '][id]" class="id" value="' . $this->idToString() . '">';
+        $sqlValues = array();
+        $sql       = 'SELECT ' . implode(',', array_keys($this->type->fields)) . ' FROM ' . $this->type->table . $this->getWhereClause($sqlValues);
+        $stmt      = prepare($sql);
+        // var_dump($stmt);
+        $result = $stmt->execute($sqlValues);
+        if (!$result) {
+            error(var_export($stmt->errorInfo(), 1));
+        }
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!$data && !$this->type->shouldInsert) {
+            error('Page not found');
+        }
+
+        foreach ($this->type->fields as $fieldKey => $field) {
+            $content .= '<div class="form-group"><label for="' . $fieldKey . '">' . $field->name . '</label> <textarea class="form-control" rows="1" name="data[' . $this->idToString() . '][' . $fieldKey . ']" id="' . $fieldKey . '">' . htmlspecialchars($data[$fieldKey]) . '</textarea></div>';
+        }
+        $content .= '</div>';
+        // var_dump($this->type);
+        echo $content;
+    }
+
+    private function getWhereClause(&$values)
+    {
+        $idFields = $this->type->id;
+        $idValues = $this->id;
+
+        $placeholders = array();
+
+        foreach ($idFields as $k => $field) {
+            $placeholders[] = $field . ' = :' . $field;
+            $values[$field] = $idValues[$k];
+        }
+
+        return ' WHERE ' . implode(' && ', $placeholders);
+    }
+
+    public function idToString()
+    {
+        return $this->type->name . ':' . implode('-', $this->id);
+    }
 
 }
