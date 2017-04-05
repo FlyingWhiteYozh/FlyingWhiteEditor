cddc0b847ce78dc37d58b50ffb69382b20c1ceb4
Yaroslav <FlyingWhiteYozh@gmail.com>
2017-04-05 16:24:09 +0300

    Добавил проверку существования основной таблицы

--- main.php
+++ main.php
@@ -5,7 +5,16 @@ class Main
 	public function __construct()
 	{
 		$this->db = Conf::getDB();
-		
+		$this->checkMainTable();
+		var_dump('test');
+	}
+
+	public function checkMainTable()
+	{
+		$page = new PageMain($this->db);
+		if (!$page->checkTable()) {
+			error('Table "' . PageMain::TABLE_NAME . '" doen\'t exist');
+		}
 	}
 
 }
--- pages/main.php
+++ pages/main.php
@@ -2,8 +2,15 @@
 
 class PageMain
 {
+	const TABLE_NAME = 'fwe_page';
+
 	public function __construct($db)
 	{
 		$this->db = $db;
+	}
+
+	public function checkTable()
+	{
+		return (bool) $this->db->query('SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = \'' . self::TABLE_NAME . '\'')->fetch();
 	}
 }
